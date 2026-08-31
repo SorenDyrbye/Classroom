255202143b99ff3c2860b471fba4bf2504f834aa
Søren Palle Dyrbye <sod004@edu.zealand.dk>
2026-08-31 11:43:05 +0200

    Tilføjet data (program.cs)

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ClassRoomNet60;
+
+KlasseRum minKlasse = new KlasseRum();
+minKlasse.KlasseNavn = "3B";
+minKlasse.SemesterStart = new DateTime(2022, 9, 1);
+minKlasse.Klasseliste.Add(new Studerende("Christian", 5, 12));
+minKlasse.Klasseliste.Add(new Studerende("Rasmus", 7, 31));
+minKlasse.Klasseliste.Add(new Studerende("Søren", 4, 26));
+
+Console.WriteLine($"Klassenavn: {minKlasse.KlasseNavn}");
+Console.WriteLine($"Semester start: {minKlasse.SemesterStart}");
+
+Console.WriteLine();
+
+Console.WriteLine("Studerende i klassen:");
+foreach (var studerende in minKlasse.Klasseliste)
+{
+    Console.WriteLine($"- {studerende.Navn} (fødselsdag: {studerende.Fødselsdag}/{studerende.Fødsesldagsmåned})");
+}
---
+++ ClassRoomNet60/Studerende.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Studerende
+    {
+        public string Navn { get;  }
+        public int Fødsesldagsmåned { get; }
+        public int Fødselsdag { get; }
+
+        public Studerende(string navn, int fødselsmåned, int fødselsdag)
+        {
+            Navn = navn;
+            Fødsesldagsmåned = fødselsmåned;
+            Fødselsdag = fødselsdag;
+        }
+    }
+
+}
