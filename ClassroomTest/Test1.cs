77048e2f15280e219448a1d0971eedd2b632d21c
Søren Palle Dyrbye <sod004@edu.zealand.dk>
2026-09-07 11:03:06 +0200

    Unittest tilføjet

---
+++ ClassroomTest/Test1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassRoomNet60;
+
+namespace ClassroomTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+      
+        [DataTestMethod]
+        [DataRow("Christian", 12, 12, "Vinter")]
+        [DataRow("Rasmus", 7, 7, "Sommer")]
+        
+
+        public void TestÅrstid(string navn, int fødselsmåned, int fødselsdag, string forventetÅrstid)
+        {
+            // Arrange
+            var studerende = new Studerende(navn, fødselsmåned, fødselsdag);
+            // Act
+            var årstid = studerende.årstid();
+            // Assert
+            Assert.AreEqual(forventetÅrstid, årstid);
+        }
+    }
+}
